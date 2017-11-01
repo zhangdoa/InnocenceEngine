47ca6b86fa70e619c936b38df33e68b619b5baf7
zhangdoa <zhangandhang@gmail.com>
2017-11-01 14:46:26 +0800

    Light system WIP: multi light source added.

--- InnocenceEngine 1.0/source/code/core/component/LightComponent.cpp
+++ InnocenceEngine 1.0/source/code/core/component/LightComponent.cpp
@@ -21,6 +21,26 @@ void LightComponent::setIntensity(float intensity)
 	m_intensity = intensity;
 }
 
+void LightComponent::setDirection(glm::vec3 direction)
+{
+	m_direction = direction;
+}
+
+void LightComponent::setConstantFactor(float constantFactor)
+{
+	m_constantFactor = constantFactor;
+}
+
+void LightComponent::setLinearFactor(float linearFactor)
+{
+	m_linearFactor = linearFactor;
+}
+
+void LightComponent::setQuadraticFactor(float quadraticFactor)
+{
+	m_quadraticFactor = quadraticFactor;
+}
+
 void LightComponent::setAmbientColor(glm::vec3 ambientColor)
 {
 	m_ambientColor = ambientColor;
@@ -47,6 +67,26 @@ float LightComponent::getIntensity() const
 	return m_intensity;
 }
 
+glm::vec3 LightComponent::getDirection() const
+{
+	return m_direction;
+}
+
+float LightComponent::getConstantFactor() const
+{
+	return m_constantFactor;
+}
+
+float LightComponent::getLinearFactor() const
+{
+	return m_linearFactor;
+}
+
+float LightComponent::getQuadraticFactor() const
+{
+	return m_quadraticFactor;
+}
+
 glm::vec3 LightComponent::getAmbientColor() const
 {
 	return m_ambientColor;
@@ -66,6 +106,10 @@ glm::vec3 LightComponent::getSpecularColor() const
 void LightComponent::initialize()
 {
 	m_intensity = 1.0f;
+	m_direction = glm::vec3(0.0f, 0.0f, 1.0f);
+	m_constantFactor = 1.0f;
+	m_linearFactor = 0.14f;
+	m_quadraticFactor = 0.07f;
 	m_ambientColor = glm::vec3(1.0f, 1.0f, 1.0f);
 	m_diffuseColor = glm::vec3(1.0f, 1.0f, 1.0f);
 	m_specularColor = glm::vec3(1.0f, 1.0f, 1.0f);
--- InnocenceEngine 1.0/source/code/core/component/LightComponent.h
+++ InnocenceEngine 1.0/source/code/core/component/LightComponent.h
@@ -11,12 +11,20 @@ public:
 
 	const lightType getLightType();
 	float getIntensity() const;
+	glm::vec3 getDirection() const;
+	float getConstantFactor() const;
+	float getLinearFactor() const;
+	float getQuadraticFactor() const;
 	glm::vec3 getAmbientColor() const;
 	glm::vec3 getDiffuseColor() const;
 	glm::vec3 getSpecularColor() const;
 
 	void setlightType(lightType lightType);
 	void setIntensity(float intensity);
+	void setDirection(glm::vec3 direction);
+	void setConstantFactor(float constantFactor);
+	void setLinearFactor(float linearFactor);
+	void setQuadraticFactor(float quadraticFactor);
 	void setAmbientColor(glm::vec3 ambientColor);
 	void setDiffuseColor(glm::vec3 diffuseColor);
 	void setSpecularColor(glm::vec3 specularColor);
@@ -24,11 +32,13 @@ public:
 private:
 	lightType m_lightType = lightType::POINT;
 	float m_intensity;
+	glm::vec3 m_direction;
+	float m_constantFactor;
+	float m_linearFactor;
+	float m_quadraticFactor;
 	glm::vec3 m_ambientColor;
 	glm::vec3 m_diffuseColor;
 	glm::vec3 m_specularColor;
-
-	glm::vec3 m_direction;
 
 	void initialize() override;
 	void update() override;
