e0c8b8a3b1061228ceb735d5cb92625af62ce00c
zhangdoa <zhangandhang@gmail.com>
2017-11-08 16:22:22 +0800

    Light system WIP: add glassware shader

--- InnocenceEngine 1.0/source/code/game/InnocenceGarden.cpp
+++ InnocenceEngine 1.0/source/code/game/InnocenceGarden.cpp
@@ -70,7 +70,7 @@ void InnocenceGarden::initialize()
 	testStaticMeshActor1.addChildComponent(&testStaticMeshComponent1);
 	SceneGraphManager::getInstance().addToRenderingQueue(&testStaticMeshComponent1);
 
-	testStaticMeshComponent2.setVisiblilityType(visiblilityType::STATIC_MESH);
+	testStaticMeshComponent2.setVisiblilityType(visiblilityType::GLASSWARE);
 	testStaticMeshActor2.addChildComponent(&testStaticMeshComponent2);
 	SceneGraphManager::getInstance().addToRenderingQueue(&testStaticMeshComponent2);
 	
@@ -84,16 +84,18 @@ void InnocenceGarden::initialize()
 	landscapeStaticMeshActor.getTransform()->setPos(glm::vec3(0.0f, 0.0f, 0.0f));
 	testStaticMeshActor1.getTransform()->setScale(glm::vec3(0.2f, 0.2f, 0.2f));
 	testStaticMeshActor1.getTransform()->setPos(glm::vec3(0.0f, 0.0f, -1.5f));
-	testStaticMeshActor2.getTransform()->setScale(glm::vec3(0.002f, 0.002f, 0.002f));
+	testStaticMeshActor2.getTransform()->setScale(glm::vec3(0.2f, 0.2f, 0.2f));
+	//testStaticMeshActor2.getTransform()->setScale(glm::vec3(0.002f, 0.002f, 0.002f));
 	testStaticMeshActor2.getTransform()->setPos(glm::vec3(0.0f, 0.0f, 3.5f));
 
 	rootActor.excute(executeMessage::INITIALIZE);
 
 	AssetManager::getInstance().importModel("nanosuit/nanosuit.obj");	
-	AssetManager::getInstance().importModel("deer.obj");
+	//AssetManager::getInstance().importModel("deer.obj");
 
 	AssetManager::getInstance().loadModel("nanosuit/nanosuit.innoModel", testStaticMeshComponent1);
-	AssetManager::getInstance().loadModel("deer.innoModel", testStaticMeshComponent2);
+	AssetManager::getInstance().loadModel("nanosuit/nanosuit.innoModel", testStaticMeshComponent2);
+	//AssetManager::getInstance().loadModel("deer.innoModel", testStaticMeshComponent2);
 
 	AssetManager::getInstance().loadTexture({ "skybox2/right.tga",
 		"skybox2/left.tga", "skybox2/top.tga", "skybox2/bottom.tga", "skybox2/back.tga", "skybox2/front.tga" }, testSkyboxComponent);
