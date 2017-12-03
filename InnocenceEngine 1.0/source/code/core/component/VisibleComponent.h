721341410c57bd07f3b3e108028b12a8dc778de4
zhangdoa <zhangandhang@gmail.com>
2017-12-03 19:44:18 +0800

    Refactor rendering system WIP: Instancing WIP

--- InnocenceEngine 1.0/source/code/core/component/VisibleComponent.h
+++ InnocenceEngine 1.0/source/code/core/component/VisibleComponent.h
@@ -12,20 +12,25 @@ public:
 	const visiblilityType& getVisiblilityType() const;
 	void setVisiblilityType(visiblilityType visiblilityType);
 
+	const meshDrawMethod& getMeshDrawMethod() const;
+	void setMeshDrawMethod(meshDrawMethod meshDrawMethod);
+
 	const textureWrapMethod& getTextureWrapMethod() const;
 	void setTextureWrapMethod(textureWrapMethod textureWrapMethod);
 
-	void addGraphicData();
-	std::vector<GraphicData>& getGraphicData();
+	void addMeshData(MeshData* meshData);
+	void addTextureData(TextureData* textureData);
 
 private:
-	std::vector<GraphicData> m_graphicData;
+	std::vector<MeshData*> m_meshDatas;
+	std::vector<TextureData*> m_textureDatas;
 
 	void initialize() override;
 	void update() override;
 	void shutdown() override;
 
 	visiblilityType m_visiblilityType = visiblilityType::INVISIBLE;
+	meshDrawMethod m_meshDrawMethod = meshDrawMethod::TRIANGLE;
 	textureWrapMethod m_textureWrapMethod = textureWrapMethod::REPEAT;
 };
 
