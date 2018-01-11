c6860dbf4423a09a4dc8c36c67f57c79eec9f06d
zhangdoa <zhangandhang@gmail.com>
2018-01-11 15:56:14 +0800

    Math & Asset loader WIP

--- InnocenceEngine 1.0/source/code/core/manager/AssetManager.h
+++ InnocenceEngine 1.0/source/code/core/manager/AssetManager.h
@@ -27,10 +27,11 @@ public:
 		return instance;
 	}
 
+	void loadAsset(const std::string& filePath);
 	void loadAsset(const std::string& filePath, VisibleComponent& visibleComponent);
 	void loadAsset(const std::string& filePath, textureType textureType, VisibleComponent& visibleComponent);
 
-	std::string loadShader(const std::string& FileName) const;
+	std::string loadShader(const std::string& fileName) const;
 
 	void loadCubeMapTextures(const std::vector<std::string>&  fileName, VisibleComponent& visibleComponent) const;
 
@@ -44,6 +45,10 @@ private:
 	void loadShaderImpl(const std::string& filePath, std::string& fileContent);
 	void loadModelImpl(const std::string& fileName, VisibleComponent& visibleComponent);
 	void loadTextureImpl(const std::string& fileName, textureType textureType, VisibleComponent& visibleComponent);
+	void loadModelImpl(const std::string& filePath);
+	void loadTextureImpl(const std::string& filePath);
+	void loadTextureFromDisk(const std::string& filePath);
+	void loadShaderImpl(const std::string& filePath);
 
 	void assignloadedModel(graphicDataMap& loadedGraphicDataMap, VisibleComponent& visibleComponent);
 
@@ -57,12 +62,18 @@ private:
 	void assignLoadedTexture(textureAssignType textureAssignType, textureDataPair& loadedTextureDataPair, VisibleComponent& visibleComponent);
 
 	textureDataID loadTextureFromDisk(const std::string & fileName, textureType textureType, textureWrapMethod textureWrapMethod);
+
 	std::unordered_map<std::string, graphicDataMap> m_loadedModelMap;
 	std::unordered_map<std::string, textureDataPair> m_loadedTextureMap;
 	std::unordered_map<std::string, int> m_supportedTextureType = { std::pair<std::string, int>("png", 0) };
 	std::unordered_map<std::string, int> m_supportedModelType = { std::pair<std::string, int>("obj",0), std::pair<std::string, int>("innoModel", 0) };
+	std::unordered_map<std::string, int> m_supportedShaderType = { std::pair<std::string, int>("sf", 0) };
 	const std::string m_textureRelativePath = "../res/textures/";
 	const std::string m_modelRelativePath = "../res/models/";
+	const std::string m_shaderRelativePath = "../res/shaders/";
+
+	std::unordered_map<std::string, void*> m_rawTextureDatas;
+	std::unordered_map<std::string, std::string> m_rawShaderDatas;
 
 	meshDataID m_UnitCubeTemplate;
 	meshDataID m_UnitSphereTemplate;
