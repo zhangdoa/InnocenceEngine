daa147d31f4e1be66f1cc806615e9f369381e605
zhangdoa <zhangandhang@gmail.com>
2018-01-14 22:54:26 +0800

    Memory pool WIP

--- InnocenceEngine 1.0/source/code/core/manager/MemoryManager.cpp
+++ InnocenceEngine 1.0/source/code/core/manager/MemoryManager.cpp
@@ -3,12 +3,21 @@
 
 void MemoryManager::setup()
 {
-	this->setup(m_maxMemoryPoolSize);
+	this->setup(m_maxPoolSize);
 }
 
 void MemoryManager::setup(unsigned int memoryPoolSize)
 {
 	m_poolMemory = ::new unsigned char[memoryPoolSize];
+	m_freePoolSize = memoryPoolSize - sizeof(Chunk);
+	m_totalPoolSize = memoryPoolSize;
+	m_freePoolSize -= sizeof(m_startBound) + sizeof(m_endBound);
+
+	Chunk freeChunk(memoryPoolSize - sizeof(Chunk) - sizeof(m_startBound) - sizeof(m_endBound));
+	freeChunk.write(m_poolMemory + sizeof(m_startBound));
+	memcpy(m_poolMemory, m_startBound, sizeof(m_startBound));
+	memcpy(m_poolMemory + memoryPoolSize - sizeof(m_startBound),
+		m_endBound, sizeof(m_endBound));
 	LogManager::getInstance().printLog("Memory is allocated " + sizeof(m_poolMemory));
 }
 
@@ -24,6 +33,8 @@ void MemoryManager::update()
 
 void MemoryManager::shutdown()
 {
+	::delete[] m_poolMemory;
+	LogManager::getInstance().printLog("MemoryManager has been shutdown.");
 }
 
 inline void * MemoryManager::allocate(unsigned int size)
