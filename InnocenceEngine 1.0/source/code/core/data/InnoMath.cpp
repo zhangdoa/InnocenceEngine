7257e7b22b1b4c9421615c5354e0a040a1f0daaf
zhangdoa <zhangandhang@gmail.com>
2018-01-25 18:01:21 +0800

    Math classes WIP

--- InnocenceEngine 1.0/source/code/core/data/InnoMath.cpp
+++ InnocenceEngine 1.0/source/code/core/data/InnoMath.cpp
@@ -39,14 +39,49 @@ vec3 vec3::add(const vec3 & rhs)
 	return vec3(x + rhs.x, y + rhs.y, z + rhs.z);
 }
 
+vec3 vec3::operator+(const vec3 & rhs)
+{
+	return vec3(x + rhs.x, y + rhs.y, z + rhs.z);
+}
+
+vec3 vec3::add(float rhs)
+{
+	return vec3(x + rhs, y + rhs, z + rhs);
+}
+
+vec3 vec3::operator+(float rhs)
+{
+	return vec3(x + rhs, y + rhs, z + rhs);
+}
+
 vec3 vec3::sub(const vec3 & rhs)
 {
 	return vec3(x - rhs.x, y - rhs.y, z - rhs.z);
 }
 
+vec3 vec3::operator-(const vec3 & rhs)
+{
+	return vec3(x - rhs.x, y - rhs.y, z - rhs.z);
+}
+
+vec3 vec3::sub(float rhs)
+{
+	return vec3(x - rhs, y - rhs, z - rhs);
+}
+
+vec3 vec3::operator-(float rhs)
+{
+	return vec3(x - rhs, y - rhs, z - rhs);
+}
+
 float vec3::dot(const vec3 & rhs)
 {
 	return x * rhs.x + y * rhs.y + z * rhs.z;
+}
+
+float vec3::dot(float rhs)
+{
+	return x * rhs + y * rhs + z * rhs;
 }
 
 vec3 vec3::cross(const vec3 & rhs)
@@ -118,4 +153,132 @@ float vec2::length()
 vec2 vec2::normalize()
 {
 	return vec2(x / length(), y / length());
+}
+
+quat::quat()
+{
+	x = 0.0f;
+	y = 0.0f;
+	z = 0.0f;
+	w = 1.0f;
+}
+
+quat::quat(float rhsX, float rhsY, float rhsZ, float rhsW)
+{
+	x = rhsX;
+	y = rhsY;
+	z = rhsZ;
+	w = rhsW;
+}
+
+quat::quat(const quat & rhs)
+{
+	x = rhs.x;
+	y = rhs.y;
+	z = rhs.z;
+	w = rhs.w;
+}
+
+quat & quat::operator=(const quat & rhs)
+{
+	x = rhs.x;
+	y = rhs.y;
+	z = rhs.z;
+	w = rhs.w;
+}
+
+quat::~quat()
+{
+}
+
+quat quat::mul(const quat & rhs)
+{
+	return quat(
+		w * rhs.x + x * rhs.w + y * rhs.z - z * rhs.y,
+		w * rhs.y - x * rhs.z + y * rhs.w + z * rhs.x,
+		w * rhs.z + x * rhs.y - y * rhs.x + z * rhs.w,
+		w * rhs.w - x * rhs.x - y * rhs.y - z * rhs.z
+	);
+}
+
+quat quat::mul(float rhs)
+{
+	return quat(
+		w * rhs + x * rhs + y * rhs - z * rhs,
+		w * rhs - x * rhs + y * rhs + z * rhs,
+		w * rhs + x * rhs - y * rhs + z * rhs,
+		w * rhs - x * rhs - y * rhs - z * rhs
+	);
+}
+
+float quat::length()
+{
+	// @TODO: replace with override impl
+	return sqrtf(x * x + y * y + z * z + w * w);
+}
+
+quat quat::normalize()
+{	
+	// @TODO: optimaze
+	return quat(x / length(), y / length(), z / length(), w / length());
+}
+
+mat4::mat4()
+{
+	m[0][0] = 0.0f;
+	m[0][1] = 0.0f;
+	m[0][2] = 0.0f;
+	m[0][3] = 0.0f;
+	m[1][0] = 0.0f;
+	m[1][1] = 0.0f;
+	m[1][2] = 0.0f;
+	m[1][3] = 0.0f;
+	m[2][0] = 0.0f;
+	m[2][1] = 0.0f;
+	m[2][2] = 0.0f;
+	m[2][3] = 0.0f;
+	m[3][0] = 0.0f;
+	m[3][1] = 0.0f;
+	m[3][2] = 0.0f;
+	m[3][3] = 0.0f;
+}
+
+mat4::mat4(const mat4 & rhs)
+{
+	m[0][0] = rhs[0][0];
+	m[0][1] = 0.0f;
+	m[0][2] = 0.0f;
+	m[0][3] = 0.0f;
+	m[1][0] = 0.0f;
+	m[1][1] = 0.0f;
+	m[1][2] = 0.0f;
+	m[1][3] = 0.0f;
+	m[2][0] = 0.0f;
+	m[2][1] = 0.0f;
+	m[2][2] = 0.0f;
+	m[2][3] = 0.0f;
+	m[3][0] = 0.0f;
+	m[3][1] = 0.0f;
+	m[3][2] = 0.0f;
+	m[3][3] = 0.0f;
+}
+
+mat4 & mat4::operator=(const mat4 & rhs)
+{
+	// TODO: insert return statement here
+}
+
+float* mat4::operator[](int index)
+{
+	return m[index];
+}
+
+mat4 mat4::mul(const mat4 & rhs)
+{
+	return mat4();
+}
+
+mat4 mat4::mul(float rhs)
+{
+	return mat4();
 }
