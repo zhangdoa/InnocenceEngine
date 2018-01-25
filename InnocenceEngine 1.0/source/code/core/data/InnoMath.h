ad83c93a23c93b0e0c5773f0663dbbf61e66fe8f
zhangdoa <zhangandhang@gmail.com>
2018-01-25 18:01:21 +0800

    Math classes WIP

--- InnocenceEngine 1.0/source/code/core/data/InnoMath.h
+++ InnocenceEngine 1.0/source/code/core/data/InnoMath.h
@@ -112,6 +112,52 @@
 //typedef __vec4 __vec3;
 //typedef __vec4 __vec2;
 
+static float sin(float angle);
+static float cos(float angle);
+static float tan(float angle);
+static float cot(float angle);
+
+// [rowIndex][columnIndex]
+class mat4
+{
+public:
+	mat4();
+	mat4(const mat4& rhs);
+	mat4& operator=(const mat4& rhs);
+	float* operator[](int index);
+	mat4 mul(const mat4& rhs);
+	// only for some semantically or literally usage
+	mat4 mul(float rhs);
+
+private:
+	float m[4][4];
+};
+
+//the w component is the last one
+class quat
+{
+public:
+	quat();
+	quat(float rhsX, float rhsY, float rhsZ, float rhsW);
+	quat(const quat& rhs);
+	quat& operator=(const quat& rhs);
+
+	~quat();
+
+	quat mul(const quat& rhs);
+	// only for some semantically or literally usage
+	quat mul(float rhs);
+	float length();
+	quat normalize();
+
+	mat4 toRotationMartix();
+
+	float x;
+	float y;
+	float z;
+	float w;
+};
+
 class vec3
 {
 public:
@@ -123,11 +169,23 @@ public:
 	~vec3();
 
 	vec3 add(const vec3& rhs);
+	vec3 operator+(const vec3& rhs);
+	vec3 add(float rhs);
+	vec3 operator+(float rhs);
 	vec3 sub(const vec3& rhs);
+	vec3 operator-(const vec3& rhs);
+	vec3 sub(float rhs);
+	vec3 operator-(float rhs);
 	float dot(const vec3& rhs);
+	float dot(float rhs);
 	vec3 cross(const vec3& rhs);
+	vec3 mul(const vec3& rhs);
+	vec3 mul(float rhs);
 	float length();
 	vec3 normalize();
+
+	mat4 toTranslationMartix();
+	mat4 toScaleMartix();
 
 	float x;
 	float y;
