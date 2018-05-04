5e52db5116773f2f560b30425c1f080f43e2f8d9
zhangdoa <zhangandhang@gmail.com>
2018-05-04 15:40:59 +0800

    Support x86-64 architecture.

--- buildForWin64VS15.bat
+++ buildForWin64VS15.bat
@@ -1,13 +1,13 @@
 mkdir build
 cd build
-cmake -G "Visual Studio 15" ../source
+cmake -G "Visual Studio 15 Win64" ../source
 msbuild InnocenceEngine.sln
 
 cd ../
 rd /s /q bin
 mkdir bin
 xcopy /s/e/y build\bin\Debug\* bin\
-xcopy /s/e/y source\external\dll\win32\* bin\
-xcopy /s/e/y source\external\dll\win32\* build\bin\Debug\
+xcopy /s/e/y source\external\dll\win64\* bin\
+xcopy /s/e/y source\external\dll\win64\* build\bin\Debug\
 xcopy /s/e/y res\* build\engine\res\
 pause
