6190fcfdf28efa420d0dc335bd4980f23fa6e9cd
Unknown <zhangandhang@gmail.com>
2018-06-14 18:48:52 +0800

    CSM WIP.

--- source/engine/component/GeometryRenderPassSingletonComponent.h
+++ source/engine/component/GeometryRenderPassSingletonComponent.h
@@ -24,16 +24,18 @@ public:
 	GLShaderProgramComponent m_geometryPassProgram;
 	GLuint m_geometryPassVertexShaderID;
 	GLuint m_geometryPassFragmentShaderID;
-	GLTextureDataComponent m_geometryPassTexture_RT0;
-	GLTextureDataComponent m_geometryPassTexture_RT1;
-	GLTextureDataComponent m_geometryPassTexture_RT2;
-	GLTextureDataComponent m_geometryPassTexture_RT3;
-	GLTextureDataComponent m_geometryPassTexture_RT4;
+	std::vector<GLTextureDataComponent> m_geometryPassTextures;
+
 	GLuint m_geometryPass_uni_prt;
 	GLuint m_geometryPass_uni_prt_previous;
 	GLuint m_geometryPass_uni_m;
-	GLuint m_geometryPass_uni_p_light;
+
+	GLuint m_geometryPass_uni_p_light_0;
+	GLuint m_geometryPass_uni_p_light_1;
+	GLuint m_geometryPass_uni_p_light_2;
+	GLuint m_geometryPass_uni_p_light_3;
 	GLuint m_geometryPass_uni_v_light;
+
 	GLuint m_geometryPass_uni_normalTexture;
 	GLuint m_geometryPass_uni_albedoTexture;
 	GLuint m_geometryPass_uni_metallicTexture;
