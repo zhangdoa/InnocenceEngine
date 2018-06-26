f369f3e4eb936540080836514455ee76a07499cb
Unknown <zhangandhang@gmail.com>
2018-06-26 17:14:58 +0800

    Almost finished CSM.

--- source/engine/component/LightComponent.cpp
+++ source/engine/component/LightComponent.cpp
@@ -2,12 +2,20 @@
 
 mat4 LightComponent::getProjectionMatrix(unsigned int cascadedLevel) const
 {
-	auto l_boundMax = m_AABBs[cascadedLevel].m_boundMax;
-	auto l_boundMin = m_AABBs[cascadedLevel].m_boundMin;
+	auto l_center = m_AABBs[cascadedLevel].m_center;
+	double l_radius = 0.0;
+	for (unsigned int i = 0; i < 8; ++i)
+	{
+		auto l_vertex = m_AABBs[cascadedLevel].m_vertices[i];
 
+		double l_distance = (l_vertex.m_pos - l_center).length();
+		l_radius = std::max(l_radius, l_distance);
+	}
+	vec4 l_maxExtents = vec4(l_radius, l_radius, l_radius, 1.0);
+	vec4 l_minExtents = l_maxExtents * (-1.0);
+	vec4 l_cascadeExtents = l_maxExtents - l_minExtents;
 	mat4 p;
-	p.initializeToOrthographicMatrix(l_boundMin.x, l_boundMax.x, l_boundMin.y, l_boundMax.y, l_boundMin.z, l_boundMax.z);
-
+	p.initializeToOrthographicMatrix(l_minExtents.x, l_maxExtents.x, l_minExtents.y, l_maxExtents.y, l_minExtents.z, l_maxExtents.z);
 	return p;
 }
 
