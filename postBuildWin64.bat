1e9d63d20cffeb2425befd36b0cc8d653d265ad9
Unknown <zhangandhang@gmail.com>
2018-06-26 21:07:54 +0800

    Split build and post-build script.

--- postBuildWin64.bat
+++ postBuildWin64.bat
@@ -1,10 +1,3 @@
-mkdir build
-cd build
-cmake -G "Visual Studio 15 Win64" ../source
-cmake -G "Visual Studio 15 Win64" ../source
-msbuild InnocenceEngine.sln
-
-cd ../
 rd /s /q bin
 mkdir bin
 xcopy /s/e/y build\bin\Debug\* bin\
