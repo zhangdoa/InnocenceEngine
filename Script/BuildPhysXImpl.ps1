4185a92d264483af8e8eeb63a7cb53bff7efb3f7
zhangdoa <zhangandhang@gmail.com>
2019-06-06 02:10:16 +0800

    WIP : Source file physical dependence
    https://trello.com/c/nnX6PNNI
    Copy PhysX's .pdb file for to allow linking with debug symbol.

--- Script/BuildPhysXImpl.ps1
+++ Script/BuildPhysXImpl.ps1
@@ -6,5 +6,6 @@ Set-Location ../Source/External/GitSubmodules/PhysX/physx
 msbuild compiler/vc15win64/PhysXSDK.sln /property:Configuration=$buildType /m
 
 $buildTypeLowerCase = $buildType.ToLower()
-xcopy /s/e/y bin\Win.x86_64.vc141.md\$buildTypeLowerCase\*.DLL ..\..\..\DLL\Win\$buildType\
-xcopy /s/e/y bin\Win.x86_64.vc141.md\$buildTypeLowerCase\*.Lib ..\..\..\Lib\Win\$buildType\
+xcopy /s/e/y bin\Win.x86_64.vc141.md\$buildTypeLowerCase\*.dll ..\..\..\DLL\Win\$buildType\
+xcopy /s/e/y bin\Win.x86_64.vc141.md\$buildTypeLowerCase\*.lib ..\..\..\Lib\Win\$buildType\
+xcopy /s/e/y bin\Win.x86_64.vc141.md\$buildTypeLowerCase\*.pdb ..\..\..\Lib\Win\$buildType\
