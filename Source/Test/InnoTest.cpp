54dee195c17e6f8523d1a1b8886ff11053a6ec4f
zhangdoa <zhangandhang@gmail.com>
2019-06-12 14:35:29 +0800

    WIP : Testing module
    https://trello.com/c/YiOYQ1wD
    Add test main entrance.

---
+++ Source/Test/InnoTest.cpp
@@ -0,0 +1,32 @@
+#include "../../Engine/Common/InnoContainer.h"
+#include "../../Engine/Common/InnoMath.h"
+#include "../../Engine/System/Core/InnoTimer.h"
+
+int main(int argc, char *argv[])
+{
+	int64_t int64 = std::numeric_limits<int64_t>::max();
+	int32_t int32 = std::numeric_limits<int32_t>::max();
+
+	auto l_StartTime = InnoTimer::GetCurrentTimeFromEpoch(TimeUnit::Microsecond);
+
+	for (size_t i = 0; i < 1024; i++)
+	{
+		auto l_StringFromInt32 = ToString(int32);
+		auto l_StringFromInt64 = ToString(int64);
+	}
+
+	auto l_Timestamp1 = InnoTimer::GetCurrentTimeFromEpoch(TimeUnit::Microsecond);
+
+	for (size_t i = 0; i < 1024; i++)
+	{
+		auto l_StringFromInt32 = std::to_string(int32);
+		auto l_StringFromInt64 = std::to_string(int64);
+	}
+
+	auto l_Timestamp2 = InnoTimer::GetCurrentTimeFromEpoch(TimeUnit::Microsecond);
+
+	auto l_SpeedRatio = float(l_Timestamp1 - l_StartTime) / float(l_Timestamp2 - l_Timestamp1);
+
+	while (1);
+	return 0;
+}
