9be1aaeb49f91c5b6c9469bcf7a43d51783fa0ee
zhangdoa <zhangandhang@gmail.com>
2019-07-03 15:37:04 +0800

    WIP : Testing module
    https://trello.com/c/YiOYQ1wD
    Replace the hard-coded test case counts.

--- Source/Test/InnoTest.cpp
+++ Source/Test/InnoTest.cpp
@@ -4,14 +4,14 @@
 #include "../Engine/Core/InnoLogger.h"
 #include "../Engine/Core/InnoMemory.h"
 
-void TestIToA()
+void TestIToA(size_t testCaseCount)
 {
 	int64_t int64 = std::numeric_limits<int64_t>::max();
 	int32_t int32 = std::numeric_limits<int32_t>::max();
 
 	auto l_StartTime = InnoTimer::GetCurrentTimeFromEpoch(TimeUnit::Microsecond);
 
-	for (size_t i = 0; i < 1024; i++)
+	for (size_t i = 0; i < testCaseCount; i++)
 	{
 		auto l_StringFromInt32 = ToString(int32);
 		auto l_StringFromInt64 = ToString(int64);
@@ -19,7 +19,7 @@ void TestIToA()
 
 	auto l_Timestamp1 = InnoTimer::GetCurrentTimeFromEpoch(TimeUnit::Microsecond);
 
-	for (size_t i = 0; i < 1024; i++)
+	for (size_t i = 0; i < testCaseCount; i++)
 	{
 		auto l_StringFromInt32 = std::to_string(int32);
 		auto l_StringFromInt64 = std::to_string(int64);
@@ -32,21 +32,21 @@ void TestIToA()
 	InnoLogger::Log(LogLevel::Success, "Custom VS STL IToA speed ratio is ", l_SpeedRatio);
 }
 
-void TestInnoArray()
+void TestInnoArray(size_t testCaseCount)
 {
 	auto l_StartTime = InnoTimer::GetCurrentTimeFromEpoch(TimeUnit::Microsecond);
 
 	InnoArray<float> l_Array;
-	l_Array.reserve(1024);
-	for (size_t i = 0; i < 1024; i++)
+	l_Array.reserve(testCaseCount);
+	for (size_t i = 0; i < testCaseCount; i++)
 	{
 		l_Array[i] = (float)i;
 	}
 	auto l_ArrayCopy = l_Array;
 
 	InnoArray<float> l_Array2;
-	l_Array2.reserve(1024);
-	for (size_t i = 0; i < 1024; i++)
+	l_Array2.reserve(testCaseCount);
+	for (size_t i = 0; i < testCaseCount; i++)
 	{
 		l_Array2.emplace_back((float)i);
 	}
@@ -54,15 +54,15 @@ void TestInnoArray()
 	auto l_Timestamp1 = InnoTimer::GetCurrentTimeFromEpoch(TimeUnit::Microsecond);
 
 	std::vector<float> l_STLArray;
-	l_STLArray.resize(1024);
-	for (size_t i = 0; i < 1024; i++)
+	l_STLArray.resize(testCaseCount);
+	for (size_t i = 0; i < testCaseCount; i++)
 	{
 		l_STLArray[i] = (float)i;
 	}
 	auto l_STLArrayCopy = l_STLArray;
 	std::vector<float> l_STLArray2;
-	l_STLArray2.resize(1024);
-	for (size_t i = 0; i < 1024; i++)
+	l_STLArray2.resize(testCaseCount);
+	for (size_t i = 0; i < testCaseCount; i++)
 	{
 		l_STLArray2.emplace_back((float)i);
 	}
@@ -74,12 +74,12 @@ void TestInnoArray()
 	InnoLogger::Log(LogLevel::Success, "Custom VS STL array container speed ratio is ", l_SpeedRatio);
 }
 
-void TestInnoMemory()
+void TestInnoMemory(size_t testCaseCount)
 {
 	std::default_random_engine l_generator;
-	std::uniform_int_distribution<unsigned int> l_randomDelta(1, 128);
+	std::uniform_int_distribution<unsigned int> l_randomDelta(1, 16384);
 
-	for (size_t i = 0; i < 32; i++)
+	for (size_t i = 0; i < testCaseCount; i++)
 	{
 		auto l_allocateSize = l_randomDelta(l_generator) * l_randomDelta(l_generator);
 		auto l_ptr = InnoMemory::Allocate(l_allocateSize);
@@ -91,9 +91,10 @@ void TestInnoMemory()
 
 int main(int argc, char *argv[])
 {
-	TestIToA();
-	TestInnoArray();
-	TestInnoMemory();
+	TestIToA(1024);
+	TestInnoArray(8192);
+	TestInnoMemory(128);
+
 	while (1);
 	return 0;
 }
