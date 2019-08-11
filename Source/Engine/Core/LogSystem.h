2d7c46805cc4af130b203b3a8c6743e5d80fd6c2
zhangdoa <zhangandhang@gmail.com>
2019-08-11 12:40:45 +0800

    WIP : Logging
    https://trello.com/c/AoRjM6Ge
    Remove redundant virtual keyword in override function declaration.

--- Source/Engine/Core/LogSystem.h
+++ Source/Engine/Core/LogSystem.h
@@ -7,35 +7,35 @@ public:
 	INNO_CLASS_CONCRETE_NON_COPYABLE(InnoLogSystem);
 
 	// Inherited via ILogSystem
-	virtual bool setup() override;
-	virtual bool initialize() override;
-	virtual bool update() override;
-	virtual bool terminate() override;
+	bool setup() override;
+	bool initialize() override;
+	bool update() override;
+	bool terminate() override;
 
-	virtual ObjectStatus getStatus() override;
+	ObjectStatus getStatus() override;
 
-	virtual void SetDefaultLogLevel(LogLevel logLevel) override;
+	void SetDefaultLogLevel(LogLevel logLevel) override;
 
-	virtual LogLevel GetDefaultLogLevel() override;
+	LogLevel GetDefaultLogLevel() override;
 
-	virtual void LogStartOfLine(LogLevel logLevel) override;
+	void LogStartOfLine(LogLevel logLevel) override;
 
-	virtual void LogEndOfLine() override;
+	void LogEndOfLine() override;
 
-	virtual void LogImpl(const void * logMessage) override;
-	virtual void LogImpl(bool logMessage) override;
-	virtual void LogImpl(uint8_t logMessage) override;
-	virtual void LogImpl(uint16_t logMessage) override;
-	virtual void LogImpl(uint32_t logMessage) override;
-	virtual void LogImpl(uint64_t logMessage) override;
-	virtual void LogImpl(int8_t logMessage) override;
-	virtual void LogImpl(int16_t logMessage) override;
-	virtual void LogImpl(int32_t logMessage) override;
-	virtual void LogImpl(int64_t logMessage) override;
-	virtual void LogImpl(float logMessage) override;
-	virtual void LogImpl(double logMessage) override;
-	virtual void LogImpl(const vec2 & logMessage) override;
-	virtual void LogImpl(const vec4 & logMessage) override;
-	virtual void LogImpl(const mat4 & logMessage) override;
-	virtual void LogImpl(const char * logMessage) override;
+	void LogImpl(const void * logMessage) override;
+	void LogImpl(bool logMessage) override;
+	void LogImpl(uint8_t logMessage) override;
+	void LogImpl(uint16_t logMessage) override;
+	void LogImpl(uint32_t logMessage) override;
+	void LogImpl(uint64_t logMessage) override;
+	void LogImpl(int8_t logMessage) override;
+	void LogImpl(int16_t logMessage) override;
+	void LogImpl(int32_t logMessage) override;
+	void LogImpl(int64_t logMessage) override;
+	void LogImpl(float logMessage) override;
+	void LogImpl(double logMessage) override;
+	void LogImpl(const vec2 & logMessage) override;
+	void LogImpl(const vec4 & logMessage) override;
+	void LogImpl(const mat4 & logMessage) override;
+	void LogImpl(const char * logMessage) override;
 };
