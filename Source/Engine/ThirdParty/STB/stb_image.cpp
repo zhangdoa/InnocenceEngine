b4bb56a8256404ca897a838635514aefae7ff182
zhangdoa <zhangandhang@gmail.com>
2019-08-22 18:41:59 +0800

    WIP : Serialization
    https://trello.com/c/nO2AFwY7
    Use custom memory allocator for stb_image loader.

--- Source/Engine/ThirdParty/STB/stb_image.cpp
+++ Source/Engine/ThirdParty/STB/stb_image.cpp
@@ -1,2 +1,6 @@
+#include "../../Core/InnoMemory.h"
+#define STBI_MALLOC(sz)           InnoMemory::Allocate(sz)
+#define STBI_REALLOC(p,newsz)     InnoMemory::Reallocate(p,newsz)
+#define STBI_FREE(p)              InnoMemory::Deallocate(p)
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
