b59a019bf28eb37dee061660708c82290b973a83
zhangdoa <zhangandhang@gmail.com>
2019-11-02 01:43:49 +0800

    WIP : Light
    https://trello.com/c/nRvwL6pV
    Reduce temporary std::vector.

--- Source/Engine/ComponentManager/LightComponentManager.cpp
+++ Source/Engine/ComponentManager/LightComponentManager.cpp
@@ -15,7 +15,7 @@ extern IModuleManager* g_pModuleManager;
 
 namespace LightComponentManagerNS
 {
-	std::vector<AABB> splitVerticesToAABBs(const std::vector<Vertex>& frustumsVertices, const std::vector<float>& splitFactors);
+	void splitVerticesToAABBs(const std::vector<Vertex>& frustumsVertices, const std::vector<float>& splitFactors, std::vector<AABB>& splitAABB);
 	void UpdateCSMData(LightComponent* rhs);
 	void UpdateColorTemperature(LightComponent* rhs);
 	void UpdateAttenuationRadius(LightComponent* rhs);
@@ -30,19 +30,23 @@ namespace LightComponentManagerNS
 	std::function<void()> f_SceneLoadingStartCallback;
 	std::function<void()> f_SceneLoadingFinishCallback;
 
-	std::vector<AABB> m_SplitAABB;
+	std::vector<float> m_CSMSplitFactors = { 0.05f, 0.25f, 0.55f, 1.0f };
+	std::vector<Vec4> m_frustumsCornerPos;
+	std::vector<Vertex> m_frustumsCornerVertices;
+	std::vector<AABB> m_SplitAABBWS;
+	std::vector<AABB> m_SplitAABBLS;
 	std::vector<Mat4> m_projectionMatrices;
 }
 
-std::vector<AABB> LightComponentManagerNS::splitVerticesToAABBs(const std::vector<Vertex>& frustumsVertices, const std::vector<float>& splitFactors)
+void LightComponentManagerNS::splitVerticesToAABBs(const std::vector<Vertex>& frustumsVertices, const std::vector<float>& splitFactors, std::vector<AABB>& splitAABB)
 {
-	std::vector<Vec4> l_frustumsCornerPos;
-	l_frustumsCornerPos.reserve(20);
+	m_frustumsCornerPos.clear();
+	splitAABB.clear();
 
 	//1. first 4 corner
 	for (size_t i = 0; i < 4; i++)
 	{
-		l_frustumsCornerPos.emplace_back(frustumsVertices[i].m_pos);
+		m_frustumsCornerPos.emplace_back(frustumsVertices[i].m_pos);
 	}
 
 	//2. other 16 corner based on the split factors
@@ -52,27 +56,25 @@ std::vector<AABB> LightComponentManagerNS::splitVerticesToAABBs(const std::vecto
 		{
 			auto l_direction = (frustumsVertices[j + 4].m_pos - frustumsVertices[j].m_pos);
 			auto l_splitPlaneCornerPos = frustumsVertices[j].m_pos + l_direction * splitFactors[i];
-			l_frustumsCornerPos.emplace_back(l_splitPlaneCornerPos);
+			m_frustumsCornerPos.emplace_back(l_splitPlaneCornerPos);
 		}
 	}
 	//https://docs.microsoft.com/windows/desktop/DxTechArts/common-techniques-to-improve-shadow-depth-maps
 	//3. assemble split frustum corners
-	std::vector<Vertex> l_frustumsCornerVertices(32);
-
 	auto l_renderingConfig = g_pModuleManager->getRenderingFrontend()->getRenderingConfig();
 
 	if (l_renderingConfig.CSMFitToScene)
 	{
 		for (size_t i = 0; i < 4; i++)
 		{
-			l_frustumsCornerVertices[i * 8].m_pos = l_frustumsCornerPos[0];
-			l_frustumsCornerVertices[i * 8 + 1].m_pos = l_frustumsCornerPos[1];
-			l_frustumsCornerVertices[i * 8 + 2].m_pos = l_frustumsCornerPos[2];
-			l_frustumsCornerVertices[i * 8 + 3].m_pos = l_frustumsCornerPos[3];
+			m_frustumsCornerVertices[i * 8].m_pos = m_frustumsCornerPos[0];
+			m_frustumsCornerVertices[i * 8 + 1].m_pos = m_frustumsCornerPos[1];
+			m_frustumsCornerVertices[i * 8 + 2].m_pos = m_frustumsCornerPos[2];
+			m_frustumsCornerVertices[i * 8 + 3].m_pos = m_frustumsCornerPos[3];
 
 			for (size_t j = 4; j < 8; j++)
 			{
-				l_frustumsCornerVertices[i * 8 + j].m_pos = l_frustumsCornerPos[i * 4 + j];
+				m_frustumsCornerVertices[i * 8 + j].m_pos = m_frustumsCornerPos[i * 4 + j];
 			}
 		}
 	}
@@ -83,36 +85,20 @@ std::vector<AABB> LightComponentManagerNS::splitVerticesToAABBs(const std::vecto
 		{
 			for (size_t j = 0; j < 8; j++)
 			{
-				l_frustumsCornerVertices[i * 8 + j].m_pos = l_frustumsCornerPos[i * 4 + j];
+				m_frustumsCornerVertices[i * 8 + j].m_pos = m_frustumsCornerPos[i * 4 + j];
 			}
 		}
 	}
 
-	//4. assemble split frustums
-	std::vector<std::vector<Vertex>> l_splitFrustums;
-	l_splitFrustums.reserve(4);
-
+	//4. generate AABBs for the split frustums
 	for (size_t i = 0; i < 4; i++)
 	{
-		auto l_splitFrustum = std::vector<Vertex>(l_frustumsCornerVertices.begin() + i * 8, l_frustumsCornerVertices.begin() + 8 + i * 8);
-		l_splitFrustums.emplace_back(l_splitFrustum);
+		splitAABB.emplace_back(InnoMath::generateAABB(&m_frustumsCornerVertices[i * 8], 8));
 	}
-
-	//5. generate AABBs for the split frustums
-	std::vector<AABB> l_AABBs;
-	l_AABBs.reserve(4);
-
-	for (size_t i = 0; i < 4; i++)
-	{
-		l_AABBs.emplace_back(InnoMath::generateAABB(&l_splitFrustums[i][0], l_splitFrustums[i].size()));
-	}
-
-	return l_AABBs;
 }
 
 void LightComponentManagerNS::UpdateCSMData(LightComponent* rhs)
 {
-	m_SplitAABB.clear();
 	m_projectionMatrices.clear();
 
 	//1. get frustum vertices in view space
@@ -208,13 +194,8 @@ void LightComponentManagerNS::UpdateCSMData(LightComponent* rhs)
 
 	auto l_frustumVerticesWS = InnoMath::viewToWorldSpace(l_frustumVerticesVS, l_tCamera, l_rCamera);
 
-	std::vector<float> l_CSMSplitFactors = { 0.05f, 0.25f, 0.55f, 1.0f };
-
 	//2. calculate AABBs in world space
-	auto l_frustumsAABBsWS = splitVerticesToAABBs(l_frustumVerticesWS, l_CSMSplitFactors);
-
-	//3. save the AABB for bound area detection
-	m_SplitAABB = std::move(l_frustumsAABBsWS);
+	splitVerticesToAABBs(l_frustumVerticesWS, m_CSMSplitFactors, m_SplitAABBWS);
 
 	//4. transform frustum vertices to light space
 	auto l_lightRotMat = GetComponent(TransformComponent, rhs->m_ParentEntity)->m_globalTransformMatrix.m_rotationMat.inverse();
@@ -222,37 +203,33 @@ void LightComponentManagerNS::UpdateCSMData(LightComponent* rhs)
 
 	for (size_t i = 0; i < l_frustumVerticesLS.size(); i++)
 	{
-		//Column-Major memory layout
 #ifdef USE_COLUMN_MAJOR_MEMORY_LAYOUT
 		l_frustumVerticesLS[i].m_pos = InnoMath::mul(l_frustumVerticesLS[i].m_pos, l_lightRotMat);
 #endif
-		//Row-Major memory layout
 #ifdef USE_ROW_MAJOR_MEMORY_LAYOUT
 		l_frustumVerticesLS[i].m_pos = InnoMath::mul(l_lightRotMat, l_frustumVerticesLS[i].m_pos);
 #endif
 	}
 
 	//5.calculate AABBs in light space
-	auto l_AABBsLS = splitVerticesToAABBs(l_frustumVerticesLS, l_CSMSplitFactors);
+	splitVerticesToAABBs(l_frustumVerticesLS, m_CSMSplitFactors, m_SplitAABBLS);
 
 	//6. extend AABB to include the bound sphere, for to eliminate rotation conflict
 	for (size_t i = 0; i < 4; i++)
 	{
-		auto sphereRadius = (l_AABBsLS[i].m_boundMax - l_AABBsLS[i].m_center).length();
-		auto l_boundMax = l_AABBsLS[i].m_center + sphereRadius;
+		auto sphereRadius = (m_SplitAABBLS[i].m_boundMax - m_SplitAABBLS[i].m_center).length();
+		auto l_boundMax = m_SplitAABBLS[i].m_center + sphereRadius;
 		l_boundMax.w = 1.0f;
-		auto l_boundMin = l_AABBsLS[i].m_center - sphereRadius;
+		auto l_boundMin = m_SplitAABBLS[i].m_center - sphereRadius;
 		l_boundMin.w = 1.0f;
-		l_AABBsLS[i] = InnoMath::generateAABB(l_boundMax, l_boundMin);
+		m_SplitAABBLS[i] = InnoMath::generateAABB(l_boundMax, l_boundMin);
 	}
 
 	//7. generate projection matrices
-	m_projectionMatrices.reserve(4);
-
 	for (size_t i = 0; i < 4; i++)
 	{
-		Vec4 l_maxExtents = l_AABBsLS[i].m_boundMax;
-		Vec4 l_minExtents = l_AABBsLS[i].m_boundMin;
+		Vec4 l_maxExtents = m_SplitAABBLS[i].m_boundMax;
+		Vec4 l_minExtents = m_SplitAABBLS[i].m_boundMin;
 
 		Mat4 p = InnoMath::generateOrthographicMatrix(l_minExtents.x, l_maxExtents.x, l_minExtents.y, l_maxExtents.y, l_minExtents.z, l_maxExtents.z);
 		m_projectionMatrices.emplace_back(p);
@@ -296,6 +273,11 @@ bool InnoLightComponentManager::Setup()
 	m_ComponentPool = InnoMemory::CreateObjectPool(sizeof(LightComponent), m_MaxComponentCount);
 	m_Components.reserve(m_MaxComponentCount);
 	m_ComponentsMap.reserve(m_MaxComponentCount);
+	m_frustumsCornerPos.reserve(20);
+	m_frustumsCornerVertices.resize(32);
+	m_SplitAABBWS.reserve(4);
+	m_SplitAABBLS.reserve(4);
+	m_projectionMatrices.reserve(4);
 
 	f_SceneLoadingStartCallback = [&]() {
 		CleanComponentContainers(LightComponent);
@@ -379,7 +361,7 @@ const LightComponent * InnoLightComponentManager::GetSun()
 
 const std::vector<AABB>& InnoLightComponentManager::GetSunSplitAABB()
 {
-	return m_SplitAABB;
+	return m_SplitAABBWS;
 }
 
 const std::vector<Mat4>& InnoLightComponentManager::GetSunProjectionMatrices()
