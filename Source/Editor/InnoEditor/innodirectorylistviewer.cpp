35d8f82a92d3b94cb10e97ee9eae8e2a2473e30d
zhangdoa <zhangandhang@gmail.com>
2019-11-07 16:48:13 +0800

    WIP : GUI Editor
    https://trello.com/c/CgePN3br
    Use the correct working directory as the relative path.

--- Source/Editor/InnoEditor/innodirectorylistviewer.cpp
+++ Source/Editor/InnoEditor/innodirectorylistviewer.cpp
@@ -30,85 +30,89 @@ QString InnoDirectoryListViewer::GetRootPath()
 
 void InnoDirectoryListViewer::SetRootPath(QString path)
 {
-	this->setRootIndex(m_fileModel->setRootPath(path));
+    this->setRootIndex(m_fileModel->setRootPath(path));
     m_rootDir = path;
 }
 
 QString InnoDirectoryListViewer::GetFilePath(QModelIndex index)
 {
-	return m_fileModel->filePath(index);
+    return m_fileModel->filePath(index);
 }
 
 QString InnoDirectoryListViewer::GetSelectionPath()
 {
-	auto indices = this->selectedIndexes();
-	return indices.size() != 0 ? m_fileModel->filePath(indices[0]) : "";
-}
+    auto indices = this->selectedIndexes();
+    return indices.size() != 0 ? m_fileModel->filePath(indices[0]) : "";
+    }
 
-void InnoDirectoryListViewer::InteractiveWithFile(QModelIndex index)
-{
-	auto l_fileInfo = m_fileModel->fileInfo(index);
+    void InnoDirectoryListViewer::InteractiveWithFile(QModelIndex index)
+    {
+    auto l_fileInfo = m_fileModel->fileInfo(index);
 
-	if (l_fileInfo.isDir())
-	{
-		this->SetRootPath(QString::fromStdString(l_fileInfo.canonicalFilePath().toStdString()));
-	}
-	else if (l_fileInfo.suffix().toStdString() == "obj"
-		|| l_fileInfo.suffix().toStdString() == "fbx"
-		|| l_fileInfo.suffix().toStdString() == "OBJ"
-		|| l_fileInfo.suffix().toStdString() == "FBX"
-		)
-	{
-		QDir l_RootDir(m_rootDir);
-		auto l_relativePath = l_RootDir.relativeFilePath(l_fileInfo.filePath());
-		switch (QMessageBox::question(
-			this,
-			tr(""),
-			tr("Convert model file?"),
+    auto l_relativeRoot = g_pModuleManager->getFileSystem()->getWorkingDirectory() + "Res//";
 
-			QMessageBox::Yes |
-			QMessageBox::No |
-			QMessageBox::Cancel,
+    if (l_fileInfo.isDir())
+    {
+        this->SetRootPath(QString::fromStdString(l_fileInfo.canonicalFilePath().toStdString()));
+    }
+    else if (l_fileInfo.suffix().toStdString() == "obj"
+             || l_fileInfo.suffix().toStdString() == "fbx"
+             || l_fileInfo.suffix().toStdString() == "OBJ"
+             || l_fileInfo.suffix().toStdString() == "FBX"
+             )
+    {
+        QDir l_RootDir(l_relativeRoot.c_str());
+        auto l_relativePath = l_RootDir.relativeFilePath(l_fileInfo.filePath());
+        switch (QMessageBox::question(
+                    this,
+                    tr(""),
+                    tr("Convert model file?"),
 
-			QMessageBox::Cancel))
-		{
-		case QMessageBox::Yes:
-			g_pModuleManager->getFileSystem()->convertModel("Res/" + l_relativePath.toStdString(), "Res/convertedAssets/");
-			break;
-		case QMessageBox::No:
-			break;
-		case QMessageBox::Cancel:
-			break;
-		default:
-			break;
-		}
-	}
-	else if (l_fileInfo.suffix().toStdString() == "InnoScene")
-	{
-		QDir l_RootDir(m_rootDir);
-		auto l_relativePath = l_RootDir.relativeFilePath(l_fileInfo.filePath());
+                    QMessageBox::Yes |
+                    QMessageBox::No |
+                    QMessageBox::Cancel,
 
-		switch (QMessageBox::question(
-			this,
-			tr(""),
-			tr("Load new scene?"),
+                    QMessageBox::Cancel))
+        {
+        case QMessageBox::Yes:
+            g_pModuleManager->getFileSystem()->convertModel("Res//" + l_relativePath.toStdString(), "Res//convertedAssets//");
+            break;
+        case QMessageBox::No:
+            g_pModuleManager->getLogSystem()->Log(LogLevel::Success, l_relativePath.toStdString().c_str());
 
-			QMessageBox::Yes |
-			QMessageBox::No |
-			QMessageBox::Cancel,
+            break;
+        case QMessageBox::Cancel:
+            break;
+        default:
+            break;
+        }
+    }
+    else if (l_fileInfo.suffix().toStdString() == "InnoScene")
+    {
+        QDir l_RootDir(l_relativeRoot.c_str());
+        auto l_relativePath = l_RootDir.relativeFilePath(l_fileInfo.filePath());
 
-			QMessageBox::Cancel))
-		{
-		case QMessageBox::Yes:
-			g_pModuleManager->getFileSystem()->loadScene("Res/" + l_relativePath.toStdString());
-			break;
-		case QMessageBox::No:
-			break;
-		case QMessageBox::Cancel:
-			break;
-		default:
-			break;
-		}
+        switch (QMessageBox::question(
+                    this,
+                    tr(""),
+                    tr("Load new scene?"),
+
+                    QMessageBox::Yes |
+                    QMessageBox::No |
+                    QMessageBox::Cancel,
+
+                    QMessageBox::Cancel))
+        {
+        case QMessageBox::Yes:
+            g_pModuleManager->getFileSystem()->loadScene("Res//" + l_relativePath.toStdString());
+            break;
+        case QMessageBox::No:
+            break;
+        case QMessageBox::Cancel:
+            break;
+        default:
+            break;
+        }
     }
 }
 
