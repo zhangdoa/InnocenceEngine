321d029145f653da4ad5acc24a7e4589078f33f3
zhangdoa <zhangandhang@gmail.com>
2020-11-11 23:00:21 +0800

    WIP : Rendering Server
    https://trello.com/c/0V9Efjkh
    Vulkan:
    Use DXC to generate SPIR-V shader.

--- Script/HLSL2SPIR-V.bat
+++
@@ -1,7 +0,0 @@
-mkdir ..\Res\Shaders\SPIRV
-cd ../Res/Shaders/SPIRV
-del /S /Q *.spv
-
-cd ../HLSL
-for %%i in (*) do glslangValidator.exe -V -t -e main -D -o ../SPIRV/%%~i.spv %%i
-pause
---
+++ Script/HLSL2SPIR-V.ps1
@@ -0,0 +1,29 @@
+mkdir ..\Res\Shaders\SPIRV
+Set-Location ../Res/Shaders/SPIRV
+Remove-Item *.spv
+
+Set-Location ../HLSL
+
+$files = Get-ChildItem
+foreach ($file in $files) {
+    $name=$file.Name
+    $extension=$file.Extension
+    $target_profile="lib_5_0"
+    if ($extension -match ".vert") {
+        $target_profile="vs_5_0"
+    } elseif ($extension -match ".tesc") {
+        $target_profile="hs_5_0"
+    } elseif ($extension -match ".tese") {
+        $target_profile="ds_5_0"
+    } elseif ($extension -match ".geom") {
+        $target_profile="gs_5_0"
+    } elseif ($extension -match ".frag") {
+        $target_profile="ps_5_0"
+    } elseif ($extension -match ".comp") {
+        $target_profile="cs_5_0"
+    }
+    
+    $genCall = "dxc -Wno-ignored-attributes -spirv -T $target_profile -E main -Fo ../SPIRV/$name.spv $file"
+    Invoke-Expression $genCall
+}
+pause
