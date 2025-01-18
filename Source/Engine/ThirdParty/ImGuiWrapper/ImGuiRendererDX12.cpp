f6f0f574352510cc9cc5fb9e9cea82961a54ff5f
zhangdoa <zhangandhang@gmail.com>
2025-01-18 20:56:38 +0100

    Implement the basic DX12 ImGui renderer (WIP).

--- Source/Engine/ThirdParty/ImGuiWrapper/ImGuiRendererDX12.cpp
+++ Source/Engine/ThirdParty/ImGuiWrapper/ImGuiRendererDX12.cpp
@@ -5,25 +5,188 @@
 #include "../../RenderingServer/DX12/DX12RenderingServer.h"
 #include "../../RenderingServer/DX12/DX12Helper.h"
 
-#include "../../Component/DX12TextureComponent.h"
+#include "../../Interface/IRenderPass.h"
 
 #include "../../Common/LogService.h"
+#include "../../Common/TaskScheduler.h"
 #include "../../Services/RenderingConfigurationService.h"
 #include "../../Services/RenderingContextService.h"
 
 #include "../../Engine.h"
 using namespace Inno;
-;
 
 namespace ImGuiRendererDX12NS
 {
-	ObjectStatus m_ObjectStatus = ObjectStatus::Terminated;
-	ComPtr<ID3D12GraphicsCommandList> m_CommandList;
+	class ImGuiRenderPass : public IRenderPass
+	{
+	public:
+		bool Setup(ISystemConfig* systemConfig) override;
+		bool Initialize() override;
+		bool Terminate() override;
+		ObjectStatus GetStatus() override;
+		bool PrepareCommandList(IRenderingContext* renderingContext) override;
+
+		RenderPassComponent* GetRenderPassComp();
+
+	private:
+		bool RenderTargetsReservationFunc();
+		bool RenderTargetsCreationFunc();
+
+		RenderPassComponent* m_RenderPassComp = nullptr;
+		ObjectStatus m_ObjectStatus = ObjectStatus::Terminated;
+	};
+
+	ImGuiRenderPass* m_RenderPass;
+
+	std::function<void()> f_SetupJob;
+	std::function<void()> f_InitializeJob;
+	std::function<void()> f_RenderJob;
+	std::function<void()> f_TerminateJob;	
+}
+
+using namespace ImGuiRendererDX12NS;
+bool ImGuiRenderPass::Setup(ISystemConfig* systemConfig)
+{
+    auto l_renderingServer = reinterpret_cast<DX12RenderingServer*>(g_Engine->getRenderingServer());
+
+    m_RenderPassComp = l_renderingServer->AddRenderPassComponent("ImGuiRenderPass/");
+
+	auto l_RenderPassDesc = g_Engine->Get<RenderingConfigurationService>()->GetDefaultRenderPassDesc();
+    l_RenderPassDesc.m_GPUEngineType = GPUEngineType::Graphics;
+	l_RenderPassDesc.m_RenderTargetCount = l_renderingServer->GetSwapChainImageCount();
+	l_RenderPassDesc.m_RenderTargetsReservationFunc = std::bind(&ImGuiRenderPass::RenderTargetsReservationFunc, this);
+	l_RenderPassDesc.m_RenderTargetsCreationFunc = std::bind(&ImGuiRenderPass::RenderTargetsCreationFunc, this);	
+	l_RenderPassDesc.m_UseMultiFrames = true;
+    l_RenderPassDesc.m_UseOutputMerger = true;
+
+    m_RenderPassComp->m_RenderPassDesc = l_RenderPassDesc;
+
+    // No resource binding layout descriptors needed for ImGui.
+
+    m_ObjectStatus = ObjectStatus::Created;
+    return true;
+}
+
+bool ImGuiRenderPass::Initialize()
+{
+    auto l_renderingServer = g_Engine->getRenderingServer();
+    l_renderingServer->InitializeRenderPassComponent(m_RenderPassComp);
+
+	// The actual rendering is called by the rendering server
+    m_RenderPassComp->m_CustomCommandsFunc = [&](ICommandList* cmdList)
+    {
+        auto dx12CmdList = reinterpret_cast<DX12CommandList*>(cmdList);
+        auto directCommandList = dx12CmdList->m_DirectCommandList.Get();
+
+        ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), directCommandList);
+    };
+
+    m_ObjectStatus = ObjectStatus::Activated;
+    return true;
+}
+
+bool ImGuiRenderPass::Terminate()
+{
+    auto l_renderingServer = g_Engine->getRenderingServer();
+    l_renderingServer->DeleteRenderPassComponent(m_RenderPassComp);
+
+    m_ObjectStatus = ObjectStatus::Terminated;
+    return true;
+}
+
+ObjectStatus ImGuiRenderPass::GetStatus()
+{
+    return m_ObjectStatus;
+}
+
+bool ImGuiRenderPass::PrepareCommandList(IRenderingContext* /*renderingContext*/)
+{
+	auto l_renderingServer = g_Engine->getRenderingServer();
+	l_renderingServer->CommandListBegin(m_RenderPassComp, 0);
+	l_renderingServer->BindRenderPassComponent(m_RenderPassComp);
+	l_renderingServer->CommandListEnd(m_RenderPassComp);
+
+    return true;
+}
+
+RenderPassComponent* ImGuiRenderPass::GetRenderPassComp()
+{
+    return m_RenderPassComp;
+}
+
+bool ImGuiRenderPass::RenderTargetsReservationFunc()
+{
+	m_RenderPassComp->m_RenderTargets.resize(m_RenderPassComp->m_RenderPassDesc.m_RenderTargetCount);
+
+    return true;
+}
+
+bool ImGuiRenderPass::RenderTargetsCreationFunc()
+{
+	auto l_renderingServer = reinterpret_cast<DX12RenderingServer*>(g_Engine->getRenderingServer());
+
+	// @TODO: This is too hard assumption for the user-side
+	auto l_userPipelineOutput = reinterpret_cast<DX12TextureComponent*>(l_renderingServer->GetUserPipelineOutput());
+
+	for (size_t i = 0; i < m_RenderPassComp->m_RenderPassDesc.m_RenderTargetCount; i++)
+	{
+		m_RenderPassComp->m_RenderTargets[i].m_Texture = l_userPipelineOutput;
+	}
+
+	return true;
 }
 
 bool ImGuiRendererDX12::Setup(ISystemConfig* systemConfig)
 {
-	ImGuiRendererDX12NS::m_ObjectStatus = ObjectStatus::Activated;
+	m_RenderPass = new ImGuiRenderPass();
+	f_SetupJob = [&]() 
+	{
+		m_RenderPass->Setup(nullptr);
+	};
+
+	f_InitializeJob = [&]() 
+	{
+		auto l_renderingServer = reinterpret_cast<DX12RenderingServer*>(g_Engine->getRenderingServer());
+		auto l_device = reinterpret_cast<ID3D12Device*>(l_renderingServer->GetDevice());
+		auto l_CSUDescHeap = reinterpret_cast<ID3D12DescriptorHeap*>(l_renderingServer->GetCSUDescHeap());
+		auto l_swapChainCount = l_renderingServer->GetSwapChainImageCount();
+
+		ImGui_ImplDX12_Init(l_device, l_renderingServer->GetSwapChainImageCount(),
+			DXGI_FORMAT_R8G8B8A8_UNORM, l_CSUDescHeap,
+			l_CSUDescHeap->GetCPUDescriptorHandleForHeapStart(),
+			l_CSUDescHeap->GetGPUDescriptorHandleForHeapStart());
+		
+		m_RenderPass->Initialize();
+	};
+
+	f_RenderJob = [&]() 
+	{
+		m_RenderPass->PrepareCommandList(nullptr);
+
+		auto l_renderingServer = g_Engine->getRenderingServer();
+		l_renderingServer->ExecuteCommandList(m_RenderPass->GetRenderPassComp(), GPUEngineType::Graphics);
+		l_renderingServer->WaitCommandQueue(m_RenderPass->GetRenderPassComp(), GPUEngineType::Compute, GPUEngineType::Graphics);
+		l_renderingServer->ExecuteCommandList(m_RenderPass->GetRenderPassComp(), GPUEngineType::Compute);
+		l_renderingServer->WaitCommandQueue(m_RenderPass->GetRenderPassComp(), GPUEngineType::Graphics, GPUEngineType::Compute);
+	};
+
+	f_TerminateJob = [&]() 
+	{
+		ImGui_ImplDX12_Shutdown();
+		m_RenderPass->Terminate();
+		
+		delete m_RenderPass;
+	};
+
+	ITask::Desc taskDesc;
+	taskDesc.m_Name = "ImGui Renderer Setup Task";
+	taskDesc.m_Type = ITask::Type::Once;
+	taskDesc.m_ThreadID = 2;
+
+	auto l_SetupTask = g_Engine->Get<TaskScheduler>()->Submit(taskDesc, f_SetupJob);
+	l_SetupTask->Activate();
+	l_SetupTask->Wait();
+
 	Log(Success, "ImGuiRendererDX12 Setup finished.");
 
 	return true;
@@ -31,18 +194,15 @@ bool ImGuiRendererDX12::Setup(ISystemConfig* systemConfig)
 
 bool ImGuiRendererDX12::Initialize()
 {
-	auto l_renderingServer = reinterpret_cast<DX12RenderingServer*>(g_Engine->getRenderingServer());
-	auto l_device = reinterpret_cast<ID3D12Device*>(l_renderingServer->GetDevice());
-	auto l_CSUDescHeap = reinterpret_cast<ID3D12DescriptorHeap*>(l_renderingServer->GetCSUDescHeap());
-	auto l_commandAllocator = reinterpret_cast<ID3D12CommandAllocator*>(l_renderingServer->GetCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, 0));
+	ITask::Desc taskDesc;
+	taskDesc.m_Name = "ImGui Renderer Initialization Task";
+	taskDesc.m_Type = ITask::Type::Once;
+	taskDesc.m_ThreadID = 2;
 
-	ImGui_ImplDX12_Init(l_device, l_renderingServer->GetSwapChainImageCount(),
-        DXGI_FORMAT_R8G8B8A8_UNORM, l_CSUDescHeap,
-        l_CSUDescHeap->GetCPUDescriptorHandleForHeapStart(),
-        l_CSUDescHeap->GetGPUDescriptorHandleForHeapStart());
+	// auto l_InitializationTask = g_Engine->Get<TaskScheduler>()->Submit(taskDesc, f_InitializeJob);
+	// l_InitializationTask->Activate();
+	// l_InitializationTask->Wait();
 
-	ImGuiRendererDX12NS::m_CommandList = DX12Helper::CreateCommandList(D3D12_COMMAND_LIST_TYPE_DIRECT, l_device, l_commandAllocator, L"ImGui_DirectCommandList");
-	ImGuiRendererDX12NS::m_CommandList->Close();
 	Log(Success, "ImGuiRendererDX12 has been initialized.");
 
 	return true;
@@ -51,24 +211,21 @@ bool ImGuiRendererDX12::Initialize()
 bool ImGuiRendererDX12::NewFrame()
 {
 	ImGui_ImplDX12_NewFrame();
+
 	return true;
 }
 
 bool ImGuiRendererDX12::Render()
 {
-	auto l_renderingServer = reinterpret_cast<DX12RenderingServer*>(g_Engine->getRenderingServer());
-	auto l_userPipelineOutput = reinterpret_cast<DX12TextureComponent*>(l_renderingServer->GetUserPipelineOutput());
-	
-	// @TODO: Implement as a render pass
-    //ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), ImGuiRendererDX12NS::m_CommandList.Get());
-	
+	f_RenderJob();
+
 	return true;
 }
 
 bool ImGuiRendererDX12::Terminate()
 {
-	ImGui_ImplDX12_Shutdown();
-	ImGuiRendererDX12NS::m_ObjectStatus = ObjectStatus::Terminated;
+	f_TerminateJob();
+
 	Log(Success, "ImGuiRendererDX12 has been terminated.");
 
 	return true;
@@ -76,7 +233,7 @@ bool ImGuiRendererDX12::Terminate()
 
 ObjectStatus ImGuiRendererDX12::GetStatus()
 {
-	return ImGuiRendererDX12NS::m_ObjectStatus;
+	return m_RenderPass->GetStatus();
 }
 
 void ImGuiRendererDX12::ShowRenderResult(RenderPassType renderPassType)
